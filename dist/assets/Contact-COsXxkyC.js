5c459d70a22cbb05fe58fadd97bbd46ad48e0c14
MahfoozMindIt <mahfoozmindit@gmail.com>
2025-04-06 23:16:50 -0700

    pages commit

---
+++ dist/assets/Contact-COsXxkyC.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Dscc4Uqj.js";const r=()=>t.jsx("div",{children:"Contact"});export{r as default};
--- dist/assets/Services-DeuFeNWl.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-a7fCn3Rn.js";const s=()=>e.jsx("div",{children:"Services"});export{s as default};
